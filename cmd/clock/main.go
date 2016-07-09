92a5ffc84bb7db9a05a3eb5ed37bef5ccf847bdf
8legd <git@8legd.com>
2016-07-09 18:33:15 +0100

    playing with heroku's custom clock processes

--- cmd/clock/main.go
+++ cmd/clock/main.go
@@ -1,13 +1,30 @@
 package main
 
 import (
-	"fmt"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
+
+	log "github.com/Sirupsen/logrus"
 )
 
 func main() {
+
+	// Catch signals so we can shutdown gracefully
+	sigCh := make(chan os.Signal)
+	signal.Notify(sigCh, syscall.SIGTERM, syscall.SIGINT)
+
+	// Kick of timer
 	ticker := time.NewTicker(time.Second * 60)
-	for t := range ticker.C {
-		fmt.Printf("TODO handle tick %v", t)
-	}
+	go func() {
+		for t := range ticker.C {
+			log.WithField("tick", t).Info("TODO handle tick")
+		}
+	}()
+
+	// Wait for signals
+	sig := <-sigCh
+	log.WithField("signal", sig).Info("Signal received. Shutting down.")
+	ticker.Stop()
 }
